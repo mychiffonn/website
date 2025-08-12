cf29e939ea5467ca0343581e7db546f1ae182ba5
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-08-12 11:32:53 -0700

    🐛 fix: link to ProfileLinks and format

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,3 +1,4 @@
 export { default as ProfileLinks } from './ProfileLinks.astro'
+export { default as Profile } from './Profile.astro'
 export { getIconForPlatform, getProcessedProfileLinks, protectEmail, PROFILE_ICON_MAP } from './helper'
 export type { ProfileLinkType, ProfileLinkConfig } from './helper'
